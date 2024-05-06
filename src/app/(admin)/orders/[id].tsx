bb4028b03314fa7175b1b5b8e8b7f540a78becce
bql <72600300+baiqinglun@users.noreply.github.com>
2024-05-06 20:40:12 +0800

    管理员更改订单状态

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,9 +1,11 @@
-import { View, Text, FlatList,StyleSheet } from 'react-native'
+import { View, Text, FlatList,StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import OrderListItem from '@/components/OrderListItem';
 import orders from '@assets/data/orders';
 import OrderItemListItem from '@/components/OrderItemListItem';
+import { OrderStatusList } from '@/types';
+import Colors from '@/constants/Colors';
 
 const OrderId = () => {
   const {id} = useLocalSearchParams();
@@ -18,6 +20,29 @@ const OrderId = () => {
         <FlatList data={order?.order_items}
           renderItem={({item})=><OrderItemListItem item={item}></OrderItemListItem>}
           contentContainerStyle={{gap:10,marginTop:20}}
+          ListFooterComponent={()=>(
+            <>
+              <Text style={{fontWeight:'600',fontSize:20,color:Colors.light.tint,marginVertical:10}}>状态：</Text>
+              <View style={{flexDirection:'row',gap:5}}>
+                {OrderStatusList.map((status)=>(
+                  <Pressable
+                    key={status}
+                    onPress={()=>{console.warn("更新成功")}}
+                    style={{
+                      borderColor:Colors.light.tint,
+                      borderWidth:1,
+                      padding:10,
+                      borderRadius:5,
+                      marginVertical:10,
+                      backgroundColor:order?.status === status ? Colors.light.tint : 'transparent'
+                    }}
+                  >
+                    <Text style={{color:order?.status === status ? "white":  Colors.light.tint}}>{status}</Text>
+                  </Pressable>
+                ))}
+              </View>
+            </>
+          )}
         />
       </View>
     </View>
--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,13 @@ export type CartItem = {
     times:number;
 }
 
+export const OrderStatusList: OrderStatus[] = [
+    'New',
+    'Cooking',
+    'Delivering',
+    'Delivered',
+  ];
+
 export type OrderStatus = 'New' | 'Cooking' | 'Delivering' | 'Delivered';
 
 export type Order = {
